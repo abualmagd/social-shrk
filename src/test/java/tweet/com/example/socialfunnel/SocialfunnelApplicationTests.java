c692f5922b8f28ba895f35baa4d025d85c7a7b70
Horus-art <ismailabualmagd@gmail.com>
2022-05-26 04:27:24 +0300

    kk

---
+++ src/test/java/tweet/com/example/socialfunnel/SocialfunnelApplicationTests.java
@@ -0,0 +1,13 @@
+package tweet.com.example.socialfunnel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialfunnelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
